0f800e042d57bdd0870773dbfe840f969d14762b
ZiwoxYT <idc@gmail.com>
2024-10-17 01:01:53 +0800

    Soon

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.discord').addEventListener('click', function() {
+    alert('Discord button clicked!');
+});
+
+document.querySelector('.store').addEventListener('click', function() {
+    alert('Store button clicked!');
+});
+
+document.querySelector('.map').addEventListener('click', function() {
+    alert('Map button clicked!');
+});
